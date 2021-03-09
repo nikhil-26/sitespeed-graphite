552f27773801b77718a5e1c7c2068a92f2b5a661
nikhil-26 <55531943+nikhil-26@users.noreply.github.com>
2021-03-09 16:09:28 +0530

    Update multi.js

--- multi.js
+++ multi.js
@@ -1,7 +1,7 @@
 module.exports = async function(context, commands) {
-  await commands.measure.start('http://beta.piana.in/sahapedia2.0/backend/users/login');
-  await commands.measure.start('http://beta.piana.in/sahapedia2.0/backend/blogs/dashboard_saha2');
-  await commands.measure.start('http://beta.piana.in/sahapedia2.0/backend/blogs/sahapedia_addPoi');
-  return commands.measure.start('http://beta.piana.in/sahapedia2.0/backend/blogs/sahapedia_articles');
+  await commands.measure.start('http://url1.com');
+  await commands.measure.start('http://url2.com');
+  await commands.measure.start('http://url3.com');
+  return commands.measure.start('http://url4.com');
 };
 
